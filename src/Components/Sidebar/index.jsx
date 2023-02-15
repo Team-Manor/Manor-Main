539312254129875bc25e28dc0dc19c40c3b01816
Destiny Aigbe <aigbedestinyic@gmail.com>
2023-02-15 17:33:20 +0100

    finished layout and setting taiwlind

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,10 +1,45 @@
+import { HomeModernIcon } from "@heroicons/react/24/outline";
+import {
+  Cog6ToothIcon,
+  QuestionMarkCircleIcon,
+} from "@heroicons/react/24/solid";
 import React from "react";
 import { Link } from "react-router-dom";
 
 function SideBar() {
+  const links = [
+    { text: "Home", icon: HomeModernIcon },
+    { text: "Explore", icon: HomeModernIcon },
+    { text: "Subscriptions", icon: HomeModernIcon },
+    { text: "LiveStreams", icon: HomeModernIcon },
+    { text: "Messaging", icon: HomeModernIcon },
+    { text: "Notifications", icon: HomeModernIcon },
+    { text: "Profile", icon: HomeModernIcon },
+    { text: "More", icon: HomeModernIcon },
+  ];
   return (
     <>
-      <div className="p-2 h-full bg-slate-400">SideBar goes here</div>
+      <div className="px-4 w-4/5 py-8 mb-6 rounded-[14px] flex flex-col items-center h-full bg-gray">
+        <div>
+          {links.map((link, i) => (
+            <Link
+              to={`/${link.text.toLowerCase()}`}
+              className="flex mb-5"
+              key={i}
+            >
+              <link.icon className="h-6" />
+              <p className="ml-2">{link.text}</p>
+            </Link>
+          ))}
+        </div>
+        <button className="bg-primary text-white h-10 w-full rounded-[10px]">
+          Connect Wallet
+        </button>
+        <div className="flex justify-around w-4/5 items-center mt-12">
+          <Cog6ToothIcon className="h-8" />
+          <QuestionMarkCircleIcon className="h-8" />
+        </div>
+      </div>
     </>
   );
 }
--- src/Components/Topbar/index.jsx
+++ src/Components/Topbar/index.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 function Topbar() {
   return (
     <>
-      <div className="p-2 w-full bg-amber-300 h-full">Topbar goes here</div>
+      <div className="p-2 pt-6 w-full h-full flex items-center justify-between">
+        <div className="flex items-center">
+          <p className="text-5xl mr-3">Manor</p>
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="min-w-[80%] flex justify-between items-center">
+          <p className="text-primary">Home</p>
+          <div className="flex items-center">
+            <input
+              type="text"
+              className="focus-visible:outline-0 border rounded-lg h-12 border-primary"
+            />
+            <div className="bg-primary h-10 rounded-lg justify-center ml-2 w-8 flex items-center">
+              <MagnifyingGlassIcon className="text-white h-6" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
